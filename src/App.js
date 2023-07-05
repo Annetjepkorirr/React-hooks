f521662701d49e43c8ecc709331685f90c5302e1
Annetjepkorirr <annetjepkorirr@gmail.com>
2023-07-06 00:08:40 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+import Product from './Product';
+function App() {
+  return (
+    <div>
+      <Login/>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
